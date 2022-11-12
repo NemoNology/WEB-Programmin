f9483056ac1045cdafcf1f2472014e7dd650c218
NemoNology <91414886+NemoNology@users.noreply.github.com>
2022-11-12 09:45:56 +0900

    Build 0.1

---
+++ Lab's/6/Scripts.js
@@ -0,0 +1,44 @@
+// var MEl = document.getElementById("MDiv");
+
+// MEl.onmousedown = onMouseDown;
+
+// function onMouseDown(e)
+// {
+	// var MX = e.ClientX, MY = e.ClientY;
+
+	// MEl.offsetLeft = 200;
+	// MEl.offsetTop = 400;
+// }
+
+
+
+// function dragElement(el)
+// {
+	// var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	// el.onmousedown = dragMouseDown;
+// }
+
+// function dragMouseDown(e)
+// {
+	// pos3 = e.clientX;
+	// pos4 = e.clientY;
+	// document.onmouseup = closeDragElement;
+	// document.onmousemove = elementDrag;
+// }
+
+// function elementDrag(e)
+// {
+	// pos1 = pos3 - e.clientX;
+    // pos2 = pos4 - e.clientY;
+    // pos3 = e.clientX;
+    // pos4 = e.clientY;
+	
+	// el.style.top = (el.offsetTop - pos2) + "px";
+    // el.style.left = (el.offsetLeft - pos1) + "px";
+// }
+
+// function closeDragElement() 
+// {
+	// document.onmouseup = null;
+	// document.onmousemove = null;
+// }
