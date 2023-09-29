f77d185247ec3f8d9cfeb2a77b52b5f9f33dffec
NemoNology <chicken.gang@mail.ru>
2023-09-30 00:42:19 +0900

    Ended almost pages & Added DB tables

---
+++ Course Work/Work/DB/Other/tables-creating.sql
@@ -0,0 +1,16 @@
+CREATE TABLE
+    IF NOT EXISTS user(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+        name VARCHAR(64) NOT NULL,
+        mail VARCHAR(128) NOT NULL,
+        password VARCHAR(64) NOT NULL
+    ) COMMENT 'User table';
+
+CREATE TABLE
+    IF NOT EXISTS joke(
+        user_id INT NOT NULL,
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+        question VARCHAR(256) NOT NULL,
+        answer VARCHAR(256) NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES user (id)
+    ) COMMENT 'Joke table';
---
+++ Course Work/Work/DB/Other/user-inserting.sql
@@ -0,0 +1,54 @@
+-- Active: 1695999082930@@127.0.0.1@3306@webdevdb
+
+INSERT INTO `user`
+VALUES (
+        DEFAULT,
+        'пушистыймишка7',
+        'kekcheburek123@gmail.com',
+        'KekCheburek123!'
+    ), (
+        DEFAULT,
+        'радужнаяфея42',
+        'fluffybear7@yahoo.com',
+        'FluffyBear7@'
+    ), (
+        DEFAULT,
+        'волшебнаяковка8',
+        'magicfairy42@hotmail.com',
+        'MagicFairy42#'
+    ), (
+        DEFAULT,
+        'мягкийпушистик99',
+        'sparklyunicorn8@outlook.com',
+        'SparklyUnicorn8$'
+    ), (
+        DEFAULT,
+        'сладкийконфетка11',
+        'sweetcandy11@gmail.com',
+        'SweetCandy11%'
+    ), (
+        DEFAULT,
+        'улыбчивыйсмайлик88',
+        'smilingsmiley88@yahoo.com',
+        'SmilingSmiley88^'
+    ), (
+        DEFAULT,
+        'искрящаясязвезда64',
+        'shimmeringstar64@outlook.com',
+        'ShimmeringStar64*'
+    ), (
+        DEFAULT,
+        'игривыйкотенок23',
+        'playfulkitten23@gmail.com',
+        'PlayfulKitten23('
+    ), (
+        DEFAULT,
+        'блистательныйбриллиант6',
+        'brilliantdiamond6@yahoo.com',
+        'BrilliantDiamond6#'
+    ), (
+        DEFAULT,
+        'таинственныйниндзя77',
+        'mysteriousninja77@hotmail.com',
+        'MysteriousNinja77&'
+    );
