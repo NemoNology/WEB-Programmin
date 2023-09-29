c37a4fac4ea7585f2e2c1d5d4d7c8f9322cceec1
NemoNology <chicken.gang@mail.ru>
2023-09-30 02:45:42 +0900

    To PHP

---
+++ Course Work/Work/Pages/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html class="fill">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>Pe-Pic</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="../Styles/main.css" />
+  <link rel="icon" href="../Assets/Icons/index.ico" />
+</head>
+
+<body class="color-gradient-main background-fixed fill">
+  <header class="container-main rounded-one color-light fill-horizontal fixed">
+    <h1>
+      <a href="/Work/Pages/index.php" class="text-style-1 text-black margin-left-medium hover-main">Pe-Pic</a>
+    </h1>
+    <div>
+      <a href="/Work/Pages/profile.php" name="pageProfile"
+        class="text-style-2 padding-small hover-sub hidden">Профиль</a>
+      <div name="pageSignUpAndIn">
+        <a class="text-style-2 padding-small hover-sub" href="./login.php">Вход</a>
+        <a class="text-style-2 padding-small hover-sub" href="./registration.php">Регистрация</a>
+      </div>
+    </div>
+  </header>
+  <div class="fill container-center">
+    <div class="container-column">
+      <div class="container-column rounded-two padding-big margin-medium color-gradient-radial-main">
+        <p class="text-style-1 text-light margin-medium">
+          Надоело забывать смешные анекдоты в форме "вопрос-ответ"?
+        </p>
+        <p class="text-style-2 text-light text-right">
+          Здесь Вы сможете сохранить все анекдоты
+        </p>
+      </div>
+      <div class="container-center">
+        <table class="fill-horizontal margin-medium color-light text-center table-main hidden">
+          <th>Вопрос</th>
+          <th>Ответ</th>
+        </table>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ Course Work/Work/Pages/registration.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html class="fill">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Pe-Pic</title>
+  <link rel="stylesheet" href="../Styles/main.css" />
+  <link rel="icon" href="../Data/Icons/index.ico" />
+</head>
+
+<body class="color-gradient-main fill background-fixed">
+  <div class="fill container-column">
+    <header class="container-main rounded-one color-light fill-horizontal">
+      <h1>
+        <a href="./index.php" class="text-style-1 margin-left-medium hover-main">Pe-Pic</a>
+      </h1>
+    </header>
+    <div class="fill container-center">
+      <div class="rounded-all-sub color-sub padding-medium">
+        <div class="container-center">
+          <p class="text-style-2 text-light margin-bottom-medium">
+            Регистрация
+          </p>
+        </div>
+        <table class="margin-bottom-medium">
+          <tr>
+            <td class="text-right">
+              <p class="text-light text-bold" name="username">Имя:</p>
+            </td>
+            <td>
+              <input type="text" name="Username" id="username" />
+            </td>
+          </tr>
+          <tr>
+            <td class="text-right">
+              <p class="text-light text-bold" name="email">Email:</p>
+            </td>
+            <td>
+              <input type="email" name="email" id="email" />
+            </td>
+          </tr>
+          <tr>
+            <td class="text-right">
+              <p class="text-light text-bold">Пароль:</p>
+            </td>
+            <td>
+              <input type="password" name="password" id="password" />
+            </td>
+          </tr>
+          <tr>
+            <td class="text-right">
+              <p class="text-light text-bold">Повторите пароль:</p>
+            </td>
+            <td>
+              <input type="password" name="repeatPassword" id="repeatPassword" />
+            </td>
+          </tr>
+        </table>
+        <div class="container-column">
+          <div class="container-center">
+            <button type="submit"
+              class="margin-top-small color-main text-light text-style-2 padding-medium rounded-all-sub border-none border-color-light hover-button-simple">
+              Зарегистрироваться
+            </button>
+          </div>
+          <div class="container-center">
+            <a href="./login.php" class="text-light margin-top-small hover-sub">Войти</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
